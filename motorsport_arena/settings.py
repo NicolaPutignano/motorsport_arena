5ae01b38e255884f52f260ccfcab83d2094f6407
NicolaPutignano <n.putignano@gmail.com>
2024-06-23 16:07:09 +0200

    Creata app arena_network

--- motorsport_arena/settings.py
+++ motorsport_arena/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
-        'USER': config('postgres'),
+        'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
