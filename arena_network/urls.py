48c678ecbbd1afe3225c39c6817aa85543ddcda3
NicolaPutignano <n.putignano@gmail.com>
2024-07-03 20:03:24 +0200

    implementata eliminazione Community (#9)

--- arena_network/urls.py
+++ arena_network/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CommunityCreateView
+from .views import CommunityCreateView, CommunityDeleteView
 
 urlpatterns = [
     path('create_community/', CommunityCreateView.as_view(), name='create_community'),
+    path('delete_community/<int:pk>/', CommunityDeleteView.as_view(), name='api_delete_community'),
 ]
--- arena_network/views.py
+++ arena_network/views.py
@@ -1,6 +1,6 @@
-from rest_framework import generics, permissions
+from django.core.mail import send_mail
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Community, CommunityMember
 from .serializers import CommunitySerializer
 
@@ -14,3 +14,27 @@ class CommunityCreateView(generics.CreateAPIView):
         community = serializer.save(created_by=self.request.user)
         CommunityMember.objects.create(user=self.request.user, community=community, role='Admin')
         return Response(serializer.data)
+
+
+class CommunityDeleteView(generics.DestroyAPIView):
+    queryset = Community.objects.all()
+    serializer_class = CommunitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        community = self.get_object()
+        user = request.user
+        try:
+            community_member = CommunityMember.objects.get(user=user, community=community, role='Admin')
+            send_mail(
+                f'Conferma di Cancellazione della Community "{community.name}"',
+                f'Ciao {user.username},\n\nLa tua community è stata cancellata con successo.',
+                'm.tucci1992@gmail.com',
+                [user.email],
+                fail_silently=False,
+            )
+        except CommunityMember.DoesNotExist:
+            return Response({"error": "You do not have permission to delete this community."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        return super().delete(request, *args, **kwargs)
