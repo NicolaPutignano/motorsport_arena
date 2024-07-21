1ae35e0d6ce906ec8def031f237ed6fbd0d66be1
darkgeggio <eugenio.altomare@gmail.com>
2024-07-21 17:33:11 +0200

    creazione view.EventDeleteView e aggiornamento urls

--- arena_events/urls.py
+++ arena_events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EventCreateView
+from .views import EventCreateView, EventDeleteView
 
 urlpatterns = [
     path('create-event/', EventCreateView.as_view(), name='create_event'),
+    path('delete-event/', EventDeleteView.as_view(), name='delete_event'),
 ]
--- arena_events/views.py
+++ arena_events/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from arena_auth.authentication import CookieJWTAuthentication
-from .models import Event
+from .models import Event, Race
 from .serializers import EventSerializer
 
 
@@ -21,3 +23,28 @@ class EventCreateView(generics.CreateAPIView):
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+class EventDeleteView(APIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [CookieJWTAuthentication]
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            event = self.get.object()
+        except Event.DoesNotExist:
+            return Response({"detail": "Evento non trovato."}, status=status.HTTP_404_NOT_FOUND)
+
+        # Controllo che l'utente connesso sia l'organizzatore dell'evento
+        if event.created_by != request.user:
+            return Response({"detail": "Non sei autorizzato a modificare questo evento."}, status=status.HTTP_403_FORBIDDEN)
+
+        # Aggiorna lo stato dell'evento
+        event.status = 'deleted'
+        event.save()
+        # Aggiorna lo stato delle gare associate
+        Race.objects.filter(event=event).update(status='deleted')
+
+        serializer = EventSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
